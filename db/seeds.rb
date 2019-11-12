2cf63549f8b118af7c3740011e88bc976e5ac41d
Camilla Kunii <milla.mk@hotmail.com>
2019-11-12 18:20:17 -0300

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(
+    title: Faker::Dessert.variety,
+    content: Faker::Dessert.flavor
+  )
+  puts 'Article.created'
+end
